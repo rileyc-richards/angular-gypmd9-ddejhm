a097afea9362f2d90bea970383b508f2dbd39050
rileyc-richards <rileycrichards@gmail.com>
2022-05-26 15:39:10 -0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
